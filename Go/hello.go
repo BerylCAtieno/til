23fc5dadbbe60e16563427fe871062361e07fc98
Beryl Atieno <107188748+BerylCAtieno@users.noreply.github.com>
2024-06-11 21:11:35 +0300

    move comments

--- Go/hello.go
+++ Go/hello.go
@@ -1,11 +1,13 @@
+// Hello World!
+// This is my first Go script
+// May this learning journey be rewarding
+
 package main
 
 import "fmt"
 
 func main() {
-	// Hello World!
-	// This is my first Go script
-	// May this learning journey be rewarding
+	
 	fmt.Print("Hello, ")
 	fmt.Print("world!")
 }
