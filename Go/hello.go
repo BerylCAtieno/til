b38305ee2b390859e8217ded3bfd9e67b1294be6
Beryl Atieno <berylatieno30@gmail.com>
2024-06-11 18:05:47 +0300

    hello go

--- Go/hello.go
+++ Go/hello.go
@@ -1,1 +1,11 @@
-// Hello World 
+package main
+
+import "fmt"
+
+func main() {
+	// Hello World!
+	// This is my first Go script
+	// May this learning journey be rewarding
+	fmt.Print("Hello, ")
+	fmt.Print("world!")
+}
